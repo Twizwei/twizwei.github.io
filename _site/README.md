7daf4a1326da2243c9a215fc9f94dc4539b34338
Twizwei <epyiran@outlook.com>
2024-06-01 19:38:31 -0400

    update template

--- _site/README.md
+++
@@ -1,4 +0,0 @@
-## My personal homepage
-This is the repo for my [personal homepage](https://twizwei.github.io/)
-
-The template is from [sharu725](https://github.com/sharu725/online-cv).
---
+++ build.py
@@ -0,0 +1,242 @@
+from pybtex.database.input import bibtex
+
+def get_personal_data():
+    name = ["Yiran", "Xu"]
+    email = "yiranx@umd.edu"
+    twitter = "Xu_Arthas"
+    github = "twizwei"
+    linkedin = "yiran-xu"
+    bio_text = f"""
+                <p>
+                    I am a Ph.D candidate at <a href="https://umd.edu/">University of Maryland, College Park</a> 
+					working with Prof. <a href="https://filebox.ece.vt.edu/~jbhuang/">Jia-Bin Huang</a>. 
+					I obtained my M.S. degree from <a href="http://www.ece.ucsd.edu/">UC San Diego</a>, advised by Prof. <a href="http://www.svcl.ucsd.edu/~nuno/">Nuno Vasconcelos</a>, 
+					and my B.E. degree from <a href="https://en.scut.edu.cn/">South China University of Technology (SCUT)</a>, advised by Prof. <a href="https://sdgc.stanford.edu/person/junbo-zhang">Junbo Zhang</a>, respectively.
+				</p>
+                <p>
+                    My research interests lie in computer vision and machine learning, with a focus on generative models and their applications,
+                    especially in <b>videos</b>.
+                </p>
+                <p>
+                    I used to intern at <sup><img src="./assets/img/adobe_logo.png" width="15"/></sup><a href="https://research.adobe.com/">Adobe Research</a> and 
+                    <sup><img src="./assets/img/snapchat-logo.svg" width="20"/></sup><a href="https://research.snap.com/">Snap Research</a> and was lucky to collaborate with many talented researchers.
+                </p>
+                <p>
+                    I am also a rookie in photography. Check out some of my <a href="./gallery.html">photos</a>!
+                </p>
+                <p>
+                    <a href="./assets/other/bio.txt" target="_blank" style="margin-right: 5px"><i class="fa-solid fa-graduation-cap"></i> Bio</a>
+                    <a href="./assets/pdf/Yiran_CV.pdf" target="_blank" style="margin-right: 5px"><i class="fa fa-address-card fa-lg"></i> CV</a>
+                    <a href="mailto:yiranx@umd.edu" style="margin-right: 5px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
+                    <a href="https://twitter.com/@Xu_Arthas" target="_blank" style="margin-right: 5px"><i class="fab fa-twitter fa-lg"></i> Twitter</a>
+                    <a href="https://scholar.google.com/citations?user=lyDM3ugAAAAJ&hl=en" target="_blank" style="margin-right: 5px"><i class="fa-solid fa-book"></i> Scholar</a>
+                    <a href="http://github.com/twizwei" target="_blank" style="margin-right: 5px"><i class="fab fa-github fa-lg"></i> Github</a>
+                    <a href="https://www.linkedin.com/in/yiran-xu-55683816b/" target="_blank" style="margin-right: 5px"><i class="fab fa-linkedin fa-lg"></i> LinkedIn</a>
+                </p>
+    """
+    footer = """
+            <div class="col-sm-12" style="">
+                <h4>Homepage Template</h4>
+                <p>
+                    The template is from <a href="https://github.com/m-niemeyer/m-niemeyer.github.io" target="_blank">Michael Niemeyer</a>. <br>
+                </p>
+            </div>
+    """
+    return name, bio_text, footer
+
+def get_author_dict():
+    return {
+        'Jia-Bin Huang': 'https://jbhuang0604.github.io/',
+        'Taesung Park': 'https://taesung.me/',
+        'Richard Zhang': 'https://richzhang.github.io/',
+        'Yang Zhou': 'https://yzhou359.github.io/',
+        'Eli Shechtman': 'https://research.adobe.com/person/eli-shechtman/',
+        'Feng Liu': 'https://pages.cs.wisc.edu/~fliu/',
+        'Difan Liu': 'https://difanliu.github.io/',
+        'Zhixin Shu': 'https://zhixinshu.github.io/',
+        'Cameron Smith': 'https://research.adobe.com/person/cameron-smith/',
+        'Seoung Wug Oh': 'https://sites.google.com/view/seoungwugoh/',
+        'Yi-Ling Qiao': 'https://ylqiao.net/',
+        'Alexander Gao': 'https://www.alexandergao.com/',
+        'Yue Feng': 'https://yuefeng21.github.io/',
+        'Ming C. Lin': 'https://www.cs.umd.edu/~lin/',
+        'Ting-Hsuan Liao': 'https://tinghliao.github.io/',
+        'Songwei Ge': 'https://songweige.github.io/',
+        'Yao-Chih Lee': 'https://yaochih.github.io/',
+        'Badour AlBahar': 'https://badouralbahar.github.io/',
+        'Nuno Vasconcelos': 'http://www.svcl.ucsd.edu/~nuno/',
+        'Xiaoyin Yang': 'https://www.linkedin.com/in/xiaoyinyang/',
+        'Lihang Gong': 'https://www.linkedin.com/in/lihang-gong/',
+        'Hsuan-Chu Lin': 'https://www.linkedin.com/in/hsuan-chu-lin/?locale=en_US',
+        'Tz-Ying Wu': 'https://gina9726.github.io/',
+        'Yunsheng Li': 'http://www.svcl.ucsd.edu/people/yunsheng/',
+        }
+
+def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Michael Niemeyer', add_links=True):
+    links = get_author_dict() if add_links else {}
+    s = ""
+    for p in persons:
+        string_part_i = ""
+        for name_part_i in p.get_part('first') + p.get_part('last'): 
+            if string_part_i != "":
+                string_part_i += " "
+            string_part_i += name_part_i
+        if string_part_i in links.keys():
+            string_part_i = f'<a href="{links[string_part_i]}" target="_blank">{string_part_i}</a>'
+        if make_bold and string_part_i == make_bold_name:
+            string_part_i = f'<span style="font-weight: bold";>{make_bold_name}</span>'
+        if p != persons[-1]:
+            string_part_i += connection
+        s += string_part_i
+    return s
+
+def get_paper_entry(entry_key, entry):
+    s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-sm-3">"""
+    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += """</div><div class="col-sm-9">"""
+
+    if 'award' in entry.fields.keys():
+        s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <span style="color: red;">({entry.fields['award']})</span><br>"""
+    else:
+        s += f"""<a href="{entry.fields['html']}" target="_blank">{entry.fields['title']}</a> <br>"""
+
+    s += f"""{generate_person_html(entry.persons['author'])} <br>"""
+    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+
+    artefacts = {'html': 'Project Page', 'pdf': 'Paper', 'supp': 'Supplemental', 'video': 'Video', 'poster': 'Poster', 'code': 'Code'}
+    i = 0
+    for (k, v) in artefacts.items():
+        if k in entry.fields.keys():
+            if i > 0:
+                s += ' / '
+            s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
+            i += 1
+        else:
+            print(f'[{entry_key}] Warning: Field {k} missing!')
+
+    cite = "<pre><code>@InProceedings{" + f"{entry_key}, \n"
+    cite += "\tauthor = {" + f"{generate_person_html(entry.persons['author'], make_bold=False, add_links=False, connection=' and ')}" + "}, \n"
+    for entr in ['title', 'booktitle', 'year']:
+        cite += f"\t{entr} = " + "{" + f"{entry.fields[entr]}" + "}, \n"
+    cite += """}</pre></code>"""
+    s += " /" + f"""<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{entry_key}" aria-expanded="false" aria-controls="collapseExample" style="margin-left: -6px; margin-top: -2px;">Expand bibtex</button><div class="collapse" id="collapse{entry_key}"><div class="card card-body">{cite}</div></div>"""
+    s += """ </div> </div> </div>"""
+    return s
+
+def get_talk_entry(entry_key, entry):
+    s = """<div style="margin-bottom: 3em;"> <div class="row"><div class="col-sm-3">"""
+    s += f"""<img src="{entry.fields['img']}" class="img-fluid img-thumbnail" alt="Project image">"""
+    s += """</div><div class="col-sm-9">"""
+    s += f"""{entry.fields['title']}<br>"""
+    s += f"""<span style="font-style: italic;">{entry.fields['booktitle']}</span>, {entry.fields['year']} <br>"""
+
+    artefacts = {'slides': 'Slides', 'video': 'Recording'}
+    i = 0
+    for (k, v) in artefacts.items():
+        if k in entry.fields.keys():
+            if i > 0:
+                s += ' / '
+            s += f"""<a href="{entry.fields[k]}" target="_blank">{v}</a>"""
+            i += 1
+        else:
+            print(f'[{entry_key}] Warning: Field {k} missing!')
+    s += """ </div> </div> </div>"""
+    return s
+
+def get_publications_html():
+    parser = bibtex.Parser()
+    bib_data = parser.parse_file('publication_list.bib')
+    keys = bib_data.entries.keys()
+    s = ""
+    for k in keys:
+        s+= get_paper_entry(k, bib_data.entries[k])
+    return s
+
+def get_talks_html():
+    parser = bibtex.Parser()
+    bib_data = parser.parse_file('talk_list.bib')
+    keys = bib_data.entries.keys()
+    s = ""
+    for k in keys:
+        s+= get_talk_entry(k, bib_data.entries[k])
+    return s
+
+def get_index_html():
+    pub = get_publications_html()
+    talks = get_talks_html()
+    name, bio_text, footer = get_personal_data()
+    s = f"""
+    <!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <title>{name[0] + ' ' + name[1]}</title>
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div class="row" style="margin-top: 3em;">
+                    <div class="col-sm-12" style="margin-bottom: 1em;">
+                    <h3 class="display-4" style="text-align: center;"><span style="font-weight: bold;">{name[0]}</span> {name[1]}</h3>
+                    </div>
+                    <br>
+                    <div class="col-md-10" style="">
+                        {bio_text}
+                    </div>
+                    <div class="col-md-2" style="">
+                        <img src="assets/img/profile.jpg" class="img-thumbnail" width="280px" alt="Profile picture">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 1em;">
+                    <div class="col-sm-12" style="">
+                        <h4>Publications</h4>
+                        {pub}
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 3em; margin-bottom: 1em;">
+                    {footer}
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div?
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"></script>
+</body>
+
+</html>
+    """
+    return s
+
+
+def write_index_html(filename='index.html'):
+    s = get_index_html()
+    with open(filename, 'w') as f:
+        f.write(s)
+    print(f'Written index content to {filename}.')
+
+if __name__ == '__main__':
+    write_index_html('index.html')
